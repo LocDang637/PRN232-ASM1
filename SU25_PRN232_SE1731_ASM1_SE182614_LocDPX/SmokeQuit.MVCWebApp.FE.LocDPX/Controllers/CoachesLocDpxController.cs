eb816185019128e557c1a825202bf7c1a28f86fc
Loc Dang <locdpxse182614@fpt.edu.vn>
2025-06-10 10:40:03 +0700

    Test 3

--- SU25_PRN232_SE1731_ASM1_SE182614_LocDPX/SmokeQuit.MVCWebApp.FE.LocDPX/Controllers/CoachesLocDpxController.cs
+++ SU25_PRN232_SE1731_ASM1_SE182614_LocDPX/SmokeQuit.MVCWebApp.FE.LocDPX/Controllers/CoachesLocDpxController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using SmokeQuit.Repositories.LocDPX.ModelExtensions;
 using SmokeQuit.Repositories.LocDPX.Models;
@@ -13,6 +14,7 @@ namespace SmokeQuit.MVCWebApp.FE.LocDPX.Controllers
     {
         private string APIEndPoint = "https://localhost:7260/api/";
 
+        // GET: CoachesLocDpx
         public async Task<IActionResult> Index(string? fullName, string? email, int? pageNumber)
         {
             var search = new SearchCoachRequest
@@ -87,6 +89,131 @@ namespace SmokeQuit.MVCWebApp.FE.LocDPX.Controllers
             return View(coach);
         }
 
-        // Additional CRUD methods...
+        // GET: CoachesLocDpx/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            CoachesLocDpx coach = null;
+
+            using (var httpClient = new HttpClient())
+            {
+                var tokenString = HttpContext.Request.Cookies.FirstOrDefault(c => c.Key == "TokenString").Value;
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + tokenString);
+
+                var response = await httpClient.GetAsync(APIEndPoint + "CoachLocDpx/" + id);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    coach = JsonConvert.DeserializeObject<CoachesLocDpx>(content);
+                }
+            }
+
+            if (coach == null) return NotFound();
+            return View(coach);
+        }
+
+        // POST: CoachesLocDpx/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, CoachesLocDpx coach)
+        {
+            if (id != coach.CoachesLocDpxid)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var tokenString = HttpContext.Request.Cookies.FirstOrDefault(c => c.Key == "TokenString").Value;
+                    httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + tokenString);
+
+                    using (var response = await httpClient.PutAsJsonAsync(APIEndPoint + "CoachLocDpx/" + id, coach))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            return RedirectToAction(nameof(Index));
+                        }
+                        else if (response.StatusCode == System.Net.HttpStatusCode.Conflict)
+                        {
+                            ModelState.AddModelError("Email", "A coach with this email already exists.");
+                        }
+                    }
+                }
+            }
+
+            return View(coach);
+        }
+
+        // GET: CoachesLocDpx/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            CoachesLocDpx coach = null;
+
+            using (var httpClient = new HttpClient())
+            {
+                var tokenString = HttpContext.Request.Cookies.FirstOrDefault(c => c.Key == "TokenString").Value;
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + tokenString);
+
+                var response = await httpClient.GetAsync(APIEndPoint + "CoachLocDpx/" + id);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    coach = JsonConvert.DeserializeObject<CoachesLocDpx>(content);
+                }
+            }
+
+            if (coach == null) return NotFound();
+            return View(coach);
+        }
+
+        // POST: CoachesLocDpx/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var tokenString = HttpContext.Request.Cookies.FirstOrDefault(c => c.Key == "TokenString").Value;
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + tokenString);
+
+                using (var response = await httpClient.DeleteAsync(APIEndPoint + "CoachLocDpx/" + id))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: CoachesLocDpx/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            CoachesLocDpx coach = null;
+
+            using (var httpClient = new HttpClient())
+            {
+                var tokenString = HttpContext.Request.Cookies.FirstOrDefault(c => c.Key == "TokenString").Value;
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + tokenString);
+
+                var response = await httpClient.GetAsync(APIEndPoint + "CoachLocDpx/" + id);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    coach = JsonConvert.DeserializeObject<CoachesLocDpx>(content);
+                }
+            }
+
+            if (coach == null) return NotFound();
+            return View(coach);
+        }
     }
 }
